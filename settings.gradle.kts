0d5eed5111f439be40d4682f15d5679adc246b3d
AlexBr <albrusen@gmail.com>
2025-06-05 23:14:50 +0300

    initial commit - add project structure

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "OzzyMapTest"
+include(":app")
+include(":core")
+include(":featureMaps")
+include(":featureMaps:domain")
+include(":featureMaps:data")
+include(":featureMaps:presentation")
+include(":core:data")
+include(":core:domain")
+include(":core:presentation")
